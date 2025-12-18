c9bbfbcf199220ab754670eef7032b2b9075147b
bartu <bartukutlu@rightsoft.co.uk>
2025-12-18 13:50:30 +0300

    padding update

--- src/components/voodvale/VoodvaleHomeFirstSection.tsx
+++ src/components/voodvale/VoodvaleHomeFirstSection.tsx
@@ -12,7 +12,7 @@ export function VoodvaleHomeFirstSection({
 }: VoodvaleHomeFirstSectionProps) {
   return (
     <div className="panel absolute left-0 top-0 will-change-transform w-full h-full z-30">
-      <motion.div className="pt-17.5 flex flex-col justify-end lg:justify-center px-5 bg-gray-300 relative h-[100vh] w-[100vw] bg-no-repeat bg-cover">
+      <motion.div className="pt-17.5 pb-[2rem] flex flex-col justify-end lg:justify-center px-5 bg-gray-300 relative h-[100vh] w-[100vw] bg-no-repeat bg-cover">
         <BackgroundMedia backgroundData={backgroundData} projectName={projectName} />
         <motion.div
           className="absolute z-20 top-0 left-0"

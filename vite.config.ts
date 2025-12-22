96cf97a47764a0ac75d267d8bfe6dbd070b257c5
bartu <bartukutlu@rightsoft.co.uk>
2025-12-22 12:53:29 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     },
   },
   build: {
+    cssCodeSplit: false,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'RightPropertyGeneratedWebUiToolkit',
